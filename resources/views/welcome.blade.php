e4e096914cadf6bc100d14bc2434596f79d3ea83
muhilan <muhilan_25@yahoo.com>
2021-03-12 17:55:53 +0800

    Change Title1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Muhilan
+                    Muhilan Nalarajah
                 </div>
 
                 <div class="links">
