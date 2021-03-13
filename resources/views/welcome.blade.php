4a4f763651df8d4044be2fcd5233d64cc488ba10
shaleen <snh3596@gmail.com>
2021-03-14 01:25:21 +0800

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    snh
+                    malala
                 </div>
 
                 <div class="links">
