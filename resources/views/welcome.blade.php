2ee5bdf310df58c60bf0662e3f2293e1242439b7
muhilan <muhilan_25@yahoo.com>
2021-03-14 01:25:53 +0800

    Update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    snh
+                    Muhilan Nalarajah
                 </div>
 
                 <div class="links">
